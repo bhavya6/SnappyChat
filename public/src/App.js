96d111f9d85e8b422f0f4736a14767cbb9fa1b94
Bhavya Kapoor <bhavyakapoor1729@gmail.com>
2022-05-24 12:43:20 +0530

    Create react app in public and installed dependencies in server

---
+++ public/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Chat from './pages/Chat';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/register" element={<Register></Register>}/>
+      <Route path="/login" element={<Login></Login>}/>
+      <Route path="/" element={<Chat></Chat>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
